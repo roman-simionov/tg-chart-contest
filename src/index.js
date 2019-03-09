1b584a0a1b982a06b9a4807f4da784dc71b57cb0
Roman Simionov <>
2019-03-09 22:28:59 +0300

    text and path elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import Renderer from "./renderer";
 
 const renderer = new Renderer(document.getElementById("test"));
-renderer.log();
+
+const text = renderer.text().setAttributes({
+    x: 10,
+    y: 10
+}).value("text");
+
+const path = renderer.path().value([[10, 10], [100, 100]]);
+
+path.renderTo(renderer.svg);
+text.renderTo(renderer.svg);
--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,76 @@
 
+class SvgWrapper {
+    /**
+     *
+     * @param {string} tagName
+     */
+    constructor(tagName) {
+        this.element = document.createElementNS("http://www.w3.org/2000/svg", tagName);
+    }
+
+    setAttributes(attributes) {
+        Object.keys(attributes).forEach(k => {
+            const value = attributes[k];
+            this.element.setAttribute(k, value);
+        });
+        return this;
+    }
+
+    setCss(styleSheet) {
+        Object.keys(styleSheet).forEach(k => {
+            const value = styleSheet[k];
+            this.element.setAttribute(k, value);
+        });
+        return this;
+    }
+
+    /**
+     *
+     * @param {SvgWrapper} svgWrapper
+     */
+    append(svgWrapper) {
+        this.element.appendChild(svgWrapper.element);
+    }
+
+    /**
+     *
+     * @param {SvgWrapper} svgWrapper
+     */
+    renderTo(svgWrapper) {
+        svgWrapper.element.appendChild(this.element);
+    }
+}
+
+class TextElement extends SvgWrapper {
+    constructor() {
+        super("text");
+    }
+
+    /**
+     *
+     * @param {string} text
+     */
+    value(text) {
+        this.element.textContent = text;
+        return this;
+    }
+}
+
+class Path extends SvgWrapper {
+    constructor() {
+        super("path");
+    }
+
+    /**
+     *
+     * @param {Array<number>} points
+     */
+    value(points) {
+        const d = `M${points.join("L")}`;
+        this.setAttributes({ d });
+        return this;
+    }
+}
 export default class Renderer {
     /**
      *
@@ -6,10 +78,23 @@ export default class Renderer {
      */
     constructor(element) {
         element.classList.add("chart");
+        this.svg = new SvgWrapper("svg").setAttributes({
+            fill: "black",
+            stroke: "black"
+        });
+        element.appendChild(this.svg.element);
     }
 
-    log() {
-        console.log(1);
+    text() {
+        return new TextElement();
+    }
+
+    path() {
+        return new Path();
+    }
+
+    createElement(tagName) {
+        return new SvgWrapper(tagName);
     }
 
 };
