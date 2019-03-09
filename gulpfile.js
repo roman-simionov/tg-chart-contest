cdc84559457e3e9daf196fd32a7cfd5a630becb8
Roman Simionov <>
2019-03-09 21:37:57 +0300

    setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require("gulp");
+const spawn = require('child_process').spawn;
+
+gulp.task("webpack", function(done) {
+    const webpack = spawn("npm", ["run", "webpack"]);
+    const data = [];
+    webpack.stdout.on("data", (d) => {
+        data.push(d.toString());
+    });
+    webpack.stderr.pipe(process.stdout);
+
+    webpack.on("exit", (code) => {
+        if (code !== 0) {
+            console.log(data.join(""));
+        }
+        done();
+    });
+});
+
+gulp.task("default", function(done) {
+    gulp.watch(["./**/*.js", "!./node_modules"], ["webpack"]);
+    done();
+});
