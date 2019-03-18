9a57bc3ec4ebb63708c36b2746466f3af541e941
Roman Simionov <>
2019-03-19 00:26:09 +0300

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,14 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    devServer: {
+        contentBase: './app'
+    },
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'app/js'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        devtoolModuleFilenameTemplate: "chart:///[resource-path]",
+        devtoolFallbackModuleFilenameTemplate: "chart:///[resource-path]?[hash]"
     }
 };
