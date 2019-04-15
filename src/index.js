29015d0e7be112a5611eba9135f63bdb8cf38ab8
Roman Simionov <>
2019-04-15 21:07:53 +0300

    remove fetch

--- src/index.js
+++ src/index.js
@@ -1,55 +1,55 @@
 import Chart from "./chart";
+import d1 from "./data/1/overview.json";
+import d2 from "./data/2/overview.json";
+import d3 from "./data/3/overview.json";
+import d4 from "./data/4/overview.json";
+import d5 from "./data/5/overview.json";
 
 window.changeTheme = function() {
     document.body.classList.toggle("dark");
 };
 
-fetch("./data/2/overview.json").then(data => data.json()).then(d => {
-    const container = document.querySelector(".charts");
-    d = d instanceof Array ? d : [d];
-    /**
-     * @type {Chart[]}
-     */
-    const charts = d.map((settings, index) => {
-            const x = settings.columns.find(c => c[0] == "x").slice(1);
-            const chart = new Chart(container, {
-                x,
-                title: `Chart#${index}`,
-                y_scaled: settings.y_scaled,
-                series: Object.keys(settings.names)
-                    .map((s) => {
-                        const name = settings.names[s];
-                        let typePrefix = "";
-                        if (settings.stacked) {
-                            typePrefix = "stacked";
-                        }
+const container = document.querySelector(".charts");
+/**
+ * @type {Chart[]}
+ */
+const charts = [d1, d2, d3, d4, d5].map((settings, index) => {
+    const x = settings.columns.find(c => c[0] == "x").slice(1);
+    const chart = new Chart(container, {
+        x,
+        title: `Chart#${index}`,
+        y_scaled: settings.y_scaled,
+        series: Object.keys(settings.names)
+            .map((s) => {
+                const name = settings.names[s];
+                let typePrefix = "";
+                if (settings.stacked) {
+                    typePrefix = "stacked";
+                }
 
-                        if (settings.percentage) {
-                            typePrefix = "percentage";
-                        }
-                        return {
-                            name,
-                            color: settings.colors[s],
-                            type: `${typePrefix}${settings.types[s]}`,
-                            x,
-                            y: settings.columns.find(c => c[0] === s).slice(1)
-                        };
-                    })
-            });
+                if (settings.percentage) {
+                    typePrefix = "percentage";
+                }
+                return {
+                    name,
+                    color: settings.colors[s],
+                    type: `${typePrefix}${settings.types[s]}`,
+                    x,
+                    y: settings.columns.find(c => c[0] === s).slice(1)
+                };
+            })
+    });
 
-            return chart;
-        });
-    container.appendChild(document.querySelector("#theme-switcher-placeholder"));
-    let timeout = null;
-    window.onresize = function() {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            charts.forEach(c => {
-                c.resize();
-            });
-        }, 30);
-    };
+    return chart;
 });
+container.appendChild(document.querySelector("#theme-switcher-placeholder"));
+let timeout = null;
 
-
-
+window.onresize = function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        charts.forEach(c => {
+            c.resize();
+        });
+    }, 30);
+};
